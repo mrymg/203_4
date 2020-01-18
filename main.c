a65f88f497f2c766ecc36c3815e11068571a8d02
ymgoz <ymgozutok@gmail.com>
2020-01-18 19:24:30 +0300

    void pointer yapısı cözüldü

--- main.c
+++ main.c
@@ -2,8 +2,13 @@
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
-#include <stdbool.h>
 
+typedef  enum  {false, true} bool;
+typedef enum {threeLeaf, twoLeaf, oneLeaf, zeroLeaf} dataType;
+int random(int range){
+    srand(time(NULL));
+    return rand()%range;
+}
 int lineCount(const char *filename)
 {
     int ch = 0;
@@ -24,10 +29,63 @@ int lineCount(const char *filename)
 
     return count;
 }
+/****defined specifications****/
+
+
+typedef struct leaf3Tree{
+    void* left;
+    void* mid;
+    void* right;
+} leaf3Tree;
+
+typedef struct leaf2Tree{
+    void* left;
+    void* right;
+} leaf2Tree;
+
+typedef struct leaf1Tree{
+    void* child;
+} leaf1Tree;
+
+typedef struct leaf{
+    char *data;
+} leaf;
+
+typedef struct Node{
+    void* node;
+    dataType type;
+}Node;
 
 
 
-void BNF_TREE();
+
+
+
+
+leaf3Tree *whichCondition(Node *root, int rnd){
+    root = malloc(sizeof(leaf3Tree));
+    Node tmpnode;
+    leaf3Tree * l3root = (leaf3Tree*)malloc(sizeof(leaf3Tree));
+
+
+
+
+
+    int thisRnd;
+    if(rnd == 0){
+        /* if random is 0 then create (<cond><set-op><cond>)*/
+        root->type = threeLeaf;
+        thisRnd = random(2);
+
+
+    }
+    else if(rnd ==1){
+        /* if random is 1 then create (<expr><rel-op><expr>) */
+
+    }
+};
+leaf3Tree *whichExpression(void);
+void showTree(Node *root);
 
 int main() {
     char *condition[2] = {"(<cond><set-op><cond>)", "(<expr><rel-op><expr>)"};
@@ -52,8 +110,8 @@ int main() {
     while ((getline(&line, &len, f_OP)) != -1) {
         opArr[j++] = strdup(strtok(line, "\r\n"));
     }
-    printf("%s", opArr[0]);
-    printf("\n");
+//    printf("%s", opArr[0]);
+//    printf("\n");
     /* ------ */
     /* ------ */
     /* ------ */
@@ -66,8 +124,8 @@ int main() {
     while ((getline(&line, &len, f_PRE_OP)) != -1) {
         pre_opArr[j++] = strdup(strtok(line, "\r\n"));
     }
-    printf("%s", pre_opArr[0]);
-    printf("\n");
+//    printf("%s", pre_opArr[0]);
+//    printf("\n");
     /* ------ */
     /* ------ */
     /* ------ */
@@ -80,8 +138,8 @@ int main() {
     while ((getline(&line, &len, f_REL_OP)) != -1) {
         rel_opArr[j++] = strdup(strtok(line, "\r\n"));
     }
-    printf("%s", rel_opArr[0]);
-    printf("\n");
+//    printf("%s", rel_opArr[0]);
+//    printf("\n");
     /* ------ */
     /* ------ */
     /* ------ */
@@ -94,8 +152,8 @@ int main() {
     while ((getline(&line, &len, f_SET_OP)) != -1) {
         set_opArr[j++] = strdup(strtok(line, "\r\n"));
     }
-    printf("%s", set_opArr[0]);
-    printf("\n");
+//    printf("%s", set_opArr[0]);
+//    printf("\n");
     /* ------ */
     /* ------ */
     /* ------ */
@@ -108,13 +166,18 @@ int main() {
     while ((getline(&line, &len, f_VAR)) != -1) {
         varArr[j++] = strdup(strtok(line, "\r\n"));
     }
-    printf("%s", varArr[0]);
-    printf("\n");
+//    printf("%s", varArr[0]);
+//    printf("\n");
     /* ------ */
     /* ------ */
     /* ------ */
 
+    int rCnd = random(2);
+    Node *root = NULL;
 
+
+    printf("if(");
+    printf(") {}\n");
 
     fclose(f_OP);
     fclose(f_PRE_OP);
